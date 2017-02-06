eec56c06bccddc523b7024edd26da6395d031ee1
Aaron Krause <aaronjkrause@gmail.com>
2017-02-06 14:19:16 -0800

    add lab example

---
+++ class-01-toolbelt-terminal-git/lab_example/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+alert('Hello');
+
+var userInput = prompt('What\'s is your name?');
+
+console.log('User answer for question one: ' + userInput);
+
+alert('Hey, ' + userInput + ', good to meet you.');
