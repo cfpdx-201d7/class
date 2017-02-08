1f339611f76a729fe000dd0b25c0e0b677ec52d9
Aaron Krause <aaronjkrause@gmail.com>
2017-02-08 12:51:24 -0800

    todays demo

---
+++ class-03-lists-loops-box_model/demo/arrays-loops.js
@@ -0,0 +1,15 @@
+var animalArray = ['duck', 'sloth', 'penguin', 'hermit crab'];
+
+var mixedArray = [true, 'string', 9, ['sub array']];
+
+animalArray[0];
+
+mixedArray[9];
+
+mixedArray[1];
+
+
+var deepArray = ['boat', 'duck', ['shark', ['submarine', ['cthulu']]], 'buoy'];
+
+var four = deepArray[2][1][1][0];
+
